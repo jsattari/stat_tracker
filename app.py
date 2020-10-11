f5facc93165eefa8fa3142d4e3adf4ef4ac9872a
jsattari <jsattari3@gmail.com>
2020-10-11 20:15:35 +0000

    adding flask deployment from pyanywhere

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, World!"
-    
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
