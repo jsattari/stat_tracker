7c54a69502157ab8e90502cfa7ce0259bdeec08c
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-11-02 19:21:30 -0800

    added more divs

--- static/myscripts.js
+++ static/myscripts.js
@@ -14,6 +14,28 @@ for (i = 1; i < 33; i++) {
 
     flex.appendChild(smallDiv);
 
+    var q = 0
+
+    for (q = 1; q < 3; q++) {
+
+        var homeDiv = document.createElement('div');
+
+        var visDiv = document.createElement('div');
+
+        homeDiv.className = 'homeDiv';
+
+        visDiv.className = 'visDiv';
+
+        homeDiv.innerHTML = 'home_score';
+
+        visDiv.innerHTML = 'visitor_score';
+
+        smallDiv.appendChild(homeDiv);
+
+        smallDiv.appendChild(visDiv);
+
+    }
+
 }
 
 document.body.appendChild(flex);
