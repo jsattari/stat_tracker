94d49d0277e17c1f38e08f21a209220090d6fb06
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-11-10 23:19:21 -0800

    Moved data pull to js file, updated css slightly to change background colors

--- static/myscripts.js
+++ static/myscripts.js
@@ -1,16 +1,26 @@
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.response);
+}
+
+data = httpGet('https://www.balldontlie.io/api/v1/games?start_date=2020-09-01&end_date=2020-09-09');
+
 var flex = document.createElement('div');
 
 flex.className = 'flex-container';
 
 // var i = 0;
 
-for (i = 1; i < 33; i++) {
+for (i = 0; i < data.data.length; i++) {
+    var cow = data.data[i]
 
     var smallDiv = document.createElement('div');
 
     smallDiv.className = 'block'
 
-    smallDiv.innerHTML = i;
+    // smallDiv.innerHTML = i;
 
     var homeDiv = document.createElement('div');
 
@@ -20,9 +30,9 @@ for (i = 1; i < 33; i++) {
 
     visDiv.className = 'visDiv';
 
-    homeDiv.innerHTML = 'home_score';
+    homeDiv.innerHTML = cow.home_team.abbreviation + ': ' + cow.home_team_score;
 
-    visDiv.innerHTML = 'visitor_score';
+    visDiv.innerHTML = cow.visitor_team.abbreviation + ': ' + cow.visitor_team_score;;
 
     smallDiv.appendChild(homeDiv);
 
