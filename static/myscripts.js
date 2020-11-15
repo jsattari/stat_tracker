b46f933480a185531b0f854d1e696ab0be6f151d
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-11-15 13:00:05 -0800

    removed node animation packages fml

--- static/myscripts.js
+++ static/myscripts.js
@@ -1,5 +1,3 @@
-import { default as anime } from '../node_modules/animejs/lib/anime.js'
-
 function httpGet(theUrl) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", theUrl, false); // false for synchronous request
@@ -60,33 +58,3 @@ for (i = 0; i < data.data.length; i++) {
 }
 
 document.body.appendChild(flex);
-
-var card = document.querySelector(".block");
-var playing = false;
-
-card.addEventListener('click', function () {
-    if (playing)
-        return;
-
-    playing = true;
-    anime({
-        targets: card,
-        scale: [{
-            value: 1
-        }, {
-            value: 1.4
-        }, {
-            value: 1,
-            delay: 250
-        }],
-        rotateY: {
-            value: '+=180',
-            delay: 200
-        },
-        easing: 'easeInOutSine',
-        duration: 400,
-        complete: function (anim) {
-            playing = false;
-        }
-    });
-});
