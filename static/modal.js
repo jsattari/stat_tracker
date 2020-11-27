aa74da05bb9b6e1b55f14922b160c9ba6d4ad1db
John Sattari <johnsattari@Johns-Air.home>
2020-11-27 15:15:59 -0800

    testing new modal.js interaction

--- static/modal.js
+++ static/modal.js
@@ -1,29 +1,8 @@
-function getModal(gameId) {
+var collection = document.getElementsByClassName('block');
 
-    data2 = httpGet('https://www.balldontlie.io/api/v1/stats?game_ids[]=' + gameId)
-    // Get the modal
-    var modal = document.getElementById("myModal");
+for (var x = 0; x < collection.length; x++) {
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+    var button = collection[x].getElementsByClassName('button-div')[0].getElementsByTagName('button')[0];
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks the button, open the modal 
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    button.addEventListener('click', console.log(collection[x].id))
 }
