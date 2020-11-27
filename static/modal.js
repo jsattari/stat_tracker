75f42c635888678fda24c190ee03b95b667ae430
John Sattari <johnsattari@Johns-Air.home>
2020-11-27 15:32:43 -0800

    testing new modal.js interaction with different function

--- static/modal.js
+++ static/modal.js
@@ -4,5 +4,7 @@ for (var x = 0; x < collection.length; x++) {
 
     var button = collection[x].getElementsByClassName('button-div')[0].getElementsByTagName('button')[0];
 
-    button.addEventListener('click', console.log(collection[x].id))
+    button.addEventListener('click', function(click) {
+        console.log(collection[x].id)
+    }, false)
 }
