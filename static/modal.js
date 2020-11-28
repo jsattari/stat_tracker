cdf1bad2401b38cae58eb9c235f45c064dcaff05
John Sattari <johnsattari@Johns-Air.home>
2020-11-27 16:42:52 -0800

    testing with set integer instead of var

--- static/modal.js
+++ static/modal.js
@@ -5,6 +5,6 @@ for (var x = 0; x < collection.length; x++) {
     var button = collection[x].getElementsByClassName('button-div')[0].getElementsByTagName('button')[0];
 
     button.addEventListener('click', function(click) {
-        console.log(collection[x].id)
+        console.log(collection[0].id)
     }, false)
 }
