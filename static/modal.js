a060dbc0888f685c3ec233940d02be936fd42567
John Sattari <johnsattari@Johns-Air.home>
2020-11-27 22:56:04 -0800

    changing method of getting button

--- static/modal.js
+++ static/modal.js
@@ -1,10 +1,11 @@
 var collection = document.getElementsByClassName('block');
 
-for (var x = 0; x < collection.length; x++) {
-
-    var button = collection[x].getElementsByClassName('button-div')[0].getElementsByTagName('button')[0];
-
-    button.addEventListener('click', function(click) {
-        console.log(collection[0].id)
-    }, false)
-}
+// Get the element, add a click listener...
+document.getElementById("myBtn").addEventListener("click", function(e) {
+	// e.target is the clicked element!
+	// If it was a list item
+	if(e.target && e.target.nodeName == "myBtn") {
+		// List item found!  Output the ID!
+		console.log(document.getElementsByClassName('block').id);
+	}
+});
