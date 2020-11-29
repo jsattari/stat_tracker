5dd166b6553b04e81c5db09b6846dde924173e66
John Sattari <johnsattari@Johns-Air.home>
2020-11-28 16:05:45 -0800

    Attempting to add modal spawning to eventlistener

--- static/modal.js
+++ static/modal.js
@@ -1,8 +1,19 @@
+// function to make api call
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.response);
+}
+
 document.querySelectorAll('button').forEach(item => {
     item.addEventListener('click', event => {
         var modal = document.getElementById('myModal');
         var span = document.getElementsByClassName("close")[0];
+        var parent = item.parentNode;
+        data2 = httpGet('https://www.balldontlie.io/api/v1/stats?game_ids[]=' + parent.id)
         modal.style.display = "block";
+        modal.innerHTML = data2.data[0].player.first_name + ' ' + data2.data[0].player.last_name
         console.log(parent.id)
     })
 })
