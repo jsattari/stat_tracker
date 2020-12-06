45e04fd44899ccce070c452c313882a060e46efb
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-12-06 09:34:45 -0800

    Updated adding rows elements in table

--- static/modal.js
+++ static/modal.js
@@ -19,14 +19,14 @@ document.querySelectorAll('button').forEach(item => {
         for (i = 0; i < data2.data.length; i++) {
             var tableBody = document.getElementsByTagName('tbody');
             var parsedData = data2.data[i];
-            let row = tableBody.insertRow();
-            let cell = row.insertCell();
+            let row = document.createElement('tr');
+            let cell = document.createElement('td');
             var names = parsedData.player.first_name + ' ' + parsedData.player.last_name;
             cell.appendChild(names);
             row.appendChild(cell);
             statsList = ['.player.first_name', '.player.last_name', '.team.abbreviation', '.pts', '.reb', '.ast']
             statsList.forEach(item => {
-                let cell = row.insertCell();
+                let cell = document.createElement('td');
                 var text = parsedData + statsList[item];
                 cell.appendChild(text);
                 row.appendChild(cell);
