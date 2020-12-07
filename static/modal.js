9fab83fe09994c2d60fdf1004a90fdadcc9656e4
John Sattari <johnsattari@Johns-MacBook-Air.local>
2020-12-06 22:32:27 -0800

    changed tableBody to call only one query selector

--- static/modal.js
+++ static/modal.js
@@ -14,43 +14,29 @@ document.querySelectorAll('button').forEach(item => {
         span.addEventListener('click', event => {
             modal.style.display = 'none'
         })
-        // let cell = document.createElement('td');
-        // var names = parsedData.player.first_name + ' ' + parsedData.player.last_name;
-        // cell.innerHTML = names;
-        // row.appendChild(cell);
-        // statsList = ['.player.first_name', '.player.last_name', '.team.abbreviation', '.pts', '.reb', '.ast']
-        document.querySelectorAll('tBody').forEach(item => {
+        var parent = item.parentNode;
 
-            var parent = item.parentNode;
-            console.log(parent);
+        var data2 = httpGet('https://www.balldontlie.io/api/v1/stats?game_ids[]=' + parent.id)
 
-            var data2 = httpGet('https://www.balldontlie.io/api/v1/stats?game_ids[]=' + parent.parentElement.parentElement.parentElement.parentElement.id)
+        var tableBody = document.querySelector('tBody');
 
-            var tableBody = document.getElementById('myTableBody');
+        for (i = 0; i < data2.data.length; i++) {
 
-            for (i = 0; i < data2.data.length; i++) {
+            var parsedData = data2.data[i];
 
-                var parsedData = data2.data[i];
+            var row = tableBody.insertRow(-1);
 
-                var row = tableBody.insertRow(-1);
+            var cellNames = row.insertCell(-1);
+            var cellTeam = row.insertCell(-1);
+            var cellPts = row.insertCell(-1);
+            var cellRebs = row.insertCell(-1);
+            var cellAsst = row.insertCell(-1);
 
-                var cellNames = row.insertCell(-1);
-                var cellTeam = row.insertCell(-1);
-                var cellPts = row.insertCell(-1);
-                var cellRebs = row.insertCell(-1);
-                var cellAsst = row.insertCell(-1);
-
-                cellNames.innerHTML = parsedData.player.first_name + ' ' + parsedData[i].player.last_name;
-                cellTeam.innerHTML = parsedData.team.abbreviation;
-                cellPts.innerHTML = parsedData.pts;
-                cellRebs.innerHTML = parsedData.reb;
-                cellAsst.innerHTML = parsedData.ast;
-            }
-
-            // tableBody.appendChild(row)
-        })
+            cellNames.innerHTML = parsedData.player.first_name + ' ' + parsedData.player.last_name;
+            cellTeam.innerHTML = parsedData.team.abbreviation;
+            cellPts.innerHTML = parsedData.pts;
+            cellRebs.innerHTML = parsedData.reb;
+            cellAsst.innerHTML = parsedData.ast;
+        }
     })
-
-    // modal.children[0].children[1].innerHTML = data2.data[0].player.first_name + ' ' + data2.data[0].player.last_name
-    //CreateTableFromJSON(data2.data, modal.children[0].children[1])
 })
